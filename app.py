47f366568129398bbe35bda168328f9adf5a2784
RR <reemaratnani@yahoo.ca>
2019-10-14 16:53:16 -0700

    update app.py

--- app.py
+++ app.py
@@ -5,17 +5,14 @@ import pandas as pd
 import pymongo
 import json
 import pickle
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 from flask import Flask, jsonify, render_template, request
 from sklearn.svm import SVC
 
 app = Flask(__name__)
-MONGO_URL = os.environ.get('MONGO_URI')
-if not MONGO_URL:
-    MONGO_URL = "mongodb://localhost:27017/account_db" 
-
-app.config["MONGO_URI"] = MONGO_URL
-mongo = PyMongo(app)
+# MONGO_URL = os.environ.get('MONGO_URI')
+# app.config["MONGO_URI"] = MONGO_URL
+# mongo = PyMongo(app)
 
 @app.route("/")
 def index():
@@ -64,60 +61,9 @@ def result():
 if __name__ == "__main__":
     app.run(debug=True)
 
-# @app.route("/gene_variants")
-# def plot_data():
-#     data = mongo.db.gene_variants
-#     output = []
-#     for x in data.find():
-#         output.append({'Class' : x['Class'],'Gene' : x['Gene'], 'Variation':x['Variation']})
-#     return jsonify(output)
 
-# @app.route("/dashboard")
-# def dashboard():
-#     return render_template("dashboard.html")
     
 
-# @app.route("/api/variation", methods=['GET'])
-# def get_api():
-#     # return render_template("api.html")
-#     api = mongo.db.gene_variants
-
-#     output = []
-#     for x in api.find():
-#         output.append({'Gene' : x['Gene'], 'Variation' : x['Variation']})
     
-#     return jsonify({'result': output})
 
-# @app.route("/api/class", methods=['GET'])
-# def get_class():
-#     # return render_template("api.html")
-#     api = mongo.db.text
-
-#     output = []
-#     for x in api.find():
-#         output.append({'Gene' : x['Gene'], 'Variation': x['Variation'],'Class' : x['Class'], 'Text': x['Text']})
-    
-#     return jsonify({'result': output})
-
-# @app.route("/api/geneData", methods=['GET'])
-# def get_geneData():
-#     # return render_template("api.html")
-#     api = mongo.db.geneData
-
-#     output = []
-#     for x in api.find():
-#         output.append({'Gene Symbol' : x['Gene Symbol'], 'Name' : x['Name'], 'Tumour Types(Somatic)' : x['Tumour Types(Somatic)'],
-#         'Tissue Type' : x['Tissue Type'], 'Molecular Genetics':x['Molecular Genetics'], 'Role in Cancer': x['Role in Cancer']})
-    
-#     return jsonify({'result': output})
-
-# @app.route("/api/geneData/<gene>")
-# def get_gene(gene):
-#     results = mongo.db.geneData.find({'Gene Symbol': gene})
-#     output = []
-#     for x in results:
-#         output.append({'Gene Symbol' : x['Gene Symbol'], 'Name' : x['Name'], 'Tumour Types(Somatic)' : x['Tumour Types(Somatic)'],
-#         'Tissue Type' : x['Tissue Type'], 'Molecular Genetics':x['Molecular Genetics'], 'Role in Cancer': x['Role in Cancer']})
-    
-#     return jsonify({'result': output})
 
