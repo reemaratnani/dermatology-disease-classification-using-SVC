052ac287edbc03855f14d0fb9d1d561e9ef6b764
RR <reemaratnani@yahoo.ca>
2019-10-15 13:01:38 -0700

    integrated stripe

--- app.py
+++ app.py
@@ -6,13 +6,23 @@ import pymongo
 import json
 import pickle
 # from flask_pymongo import PyMongo
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 from sklearn.svm import SVC
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-# MONGO_URL = os.environ.get('MONGO_URI')
-# app.config["MONGO_URI"] = MONGO_URL
-# mongo = PyMongo(app)
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    pwd = db.Column(db.String(30), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
 
 @app.route("/")
 def index():
@@ -26,6 +36,9 @@ def form():
 def thanks():
     return render_template("thanks.html")
 
+@app.route('/charge', methods=['POST', 'GET'])
+def pay():
+	return redirect(url_for('form'))
 
 #prediction function
 def ValuePredictor(to_predict_list):
@@ -57,6 +70,19 @@ def result():
             prediction = "Class 6: Pityriasis Rubra Pilaris"
             
         return render_template("form.html",result=prediction)
+
+
+# @app.route("/contact", methods = ['GET', 'POST'])
+# def contact():
+#     if(request.method == 'POST'):
+#         name = request.form.get('name')
+#         email = request.form.get('email')
+#         pwd = request.form.get('pwd')
+#         entry = Contact(name=name, phone_num = phone, msg = message, date= datetime.now(),email = email )
+#         db.session.add(entry)
+#         db.session.commit()
+
+#     return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
